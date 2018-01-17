c016cc54e46b0543dd0805f84e55dc8b1f1ed5a1
Novalis YAO <kyaonovalis@gmail.com>
2018-01-17 12:23:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-if (string.toLowerCase() === "hello!") {
+if (string === string.toLowerCase()) {
     return "I can\'t hear you!";
-}  else if (string.toUpperCase() === "HELLO!") {
+} if (string === string.toUpperCase()) {
     return "YES INDEED!";
-} else if (string === "I love you, Grandma.") {
+} if (string === "I love you, Grandma.") {
     return "I love you, too.";
 }}
+if (string === string.toLowerCase) ?
